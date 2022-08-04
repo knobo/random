b89a3d1fa3b010fd2ad1e4b533c3d5b79b607bb7
Knut Olav Bøhmer <bohmer@gmail.com>
2022-08-04 11:23:51 +0200

    added separator

--- rand.cpp
+++ rand.cpp
@@ -1,10 +1,36 @@
 #include "argparse.hpp"
 #include <stdio.h>  /* printf, scanf, puts, NULL */
 #include <stdlib.h> /* srand, rand */
-#include <time.h>   /* time */
 
 #include <cctype>
 #include <random>
+
+
+std::string parse(const std::string& s)
+{
+  std::stringstream  ss{""};
+
+  for(size_t i = 0; i < s.length(); i++)
+  {
+    if (s.at(i) == '\\')
+    {
+      switch(s.at(i + 1))
+      {
+        case 'n':  ss << "\n"; i++; break;
+        case 'r':  ss << "\r"; i++; break;
+        case '"':  ss << "\""; i++; break;
+        case 't':  ss << "\t"; i++; break;
+        default:   ss << "\\";      break;
+      }
+    }
+    else
+    {
+      ss << s.at(i);
+    }
+  }
+
+  return ss.str();
+}
 
 int main(int argc, char *argv[]) {
 
@@ -12,20 +38,28 @@ int main(int argc, char *argv[]) {
   int i = 1;
   int start = 0;
   int stop = 10;
+  std::string separator = " ";
 
   program.add_argument("-c", "--count")
-      .help("Number of random numbers")
-      .action([=, &i](const std::string &value) { i = std::stoi(value); });
+    .help("Number of random numbers")
+    .action([=, &i](const std::string &value) { i = std::stoi(value); });
 
   program.add_argument("-a", "--start")
-      .help("start random from")
-      .action(
-          [=, &start](const std::string &value) { start = std::stoi(value); });
+    .help("start random from")
+    .action(
+      [=, &start](const std::string &value) { start = std::stoi(value); });
 
   program.add_argument("-z", "--end")
-      .help("largest random number")
-      .action(
-          [=, &stop](const std::string &value) { stop = std::stoi(value); });
+    .help("largest random number")
+    .action(
+      [=, &stop](const std::string &value) { stop = std::stoi(value); });
+
+  program.add_argument("-s", "--separator")
+    .help("The separator to use between numbers")
+    .action(
+      [=, &separator](const std::string &value) {
+        separator = parse(value);
+      });
 
   try {
     program.parse_args(argc, argv);
@@ -36,8 +70,9 @@ int main(int argc, char *argv[]) {
   }
 
   std::random_device rd; // uses RDRND or /dev/urandom
-  std::mt19937 gen(rd());                           //
+  std::mt19937 gen(rd());
   std::uniform_int_distribution<int> dist(start, stop);
   for (int n = 0; n < i; ++n)
-    std::cout << dist(rd) << std::endl;
+    if(n + 1 == i) std::cout << dist(rd);
+    else std::cout << dist(rd) << separator;
 }
